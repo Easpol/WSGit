6f0627e48705a6e29347dc35e25e06dcc1e1d648
Raúl Moreno <raulmro@hotmail.es>
2022-02-10 21:15:09 +0100

    Creada persona imprimida por pantalla

--- 01_PrimerProyectoGit/src/git/main/Main.java
+++ 01_PrimerProyectoGit/src/git/main/Main.java
@@ -1,9 +1,17 @@
 package git.main;
 
+import git.entidad.Persona;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos a nuestras versiones con Git");
+		
+		Persona p = new Persona();
+		p.setNombre("Steve Rogers");
+		p.setEdad (45);
+		
+		System.out.println (p);
 	}
 
 }
